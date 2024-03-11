d2e10d597d484c4a482381fd3dda68ca90787615
INGSebastianMarciales <sebastianmarciales40@gmail.com>
2024-03-10 22:57:40 -0500

    Se agrego el link de la master class y el video

--- Js/script.js
+++ Js/script.js
@@ -1,23 +1,6 @@
 
 // function VerMasBotonRegistrarse() {
-//     fetch('vermas.html')
-//       .then(response => {
-//         // Verifica si la respuesta es exitosa (status 200)
-//         if (!response.ok) {
-//           throw new Error('La respuesta no fue exitosa');
-//         }
-//         // Devuelve el contenido de la respuesta como texto
-//         return response.text();
-//       })
-//       .then(data => {
-//         // Carga el contenido de pagina2.html en el navegador
-//         document.open();
-//         document.write(data);
-//         document.close();
-//       })
-//       .catch(error => {
-//         console.error('Error:', error);
-//       });
+
 //   }
 
 //   function VolverIndex() {  
@@ -94,6 +77,26 @@ function CerrarModal(){
   let modal = document.getElementById('ModalRegistro');
   modal.style.display = "none";
 }
-  function RedirigirWhatsApp(){
-    window.location.href = 'https://api.whatsapp.com/send?phone=573167711746&text=Hola, ¿deseo agendar una video consulta?';
-  }
+function RedirigirWhatsApp(){
+  window.location.href = 'https://api.whatsapp.com/send?phone=573167711746&text=Hola, ¿deseo agendar una video consulta?';
+}
+function RegistrarDatosNotificaciones(){
+  fetch('masterClass.html')
+  .then(response => {
+    // Verifica si la respuesta es exitosa (status 200)
+    if (!response.ok) {
+      throw new Error('La respuesta no fue exitosa');
+    }
+    // Devuelve el contenido de la respuesta como texto
+    return response.text();
+  })
+  .then(data => {
+    // Carga el contenido de pagina2.html en el navegador
+    document.open();
+    document.write(data);
+    document.close();
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
